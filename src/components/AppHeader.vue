ea0b36d07cf54a6a91055f265be928d5b202c225
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-04-01 19:19:06 +0200

    modificata la navbar con bootstrap

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,29 @@
 <template>
-  <header class="d-flex justify-content-between">
-    <h1>NEXGEN</h1>
-    <div class="text-uppercase">
-      <a href="">home</a><a href="">services</a><a href="">skills</a
-      ><a href="">get in touch</a>
+  <nav class="navbar navbar-expand justify-content-between">
+    <h1 class="navbar-brand">NEXGEN</h1>
+    <div>
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0 text-uppercase">
+        <li class="nav-item">
+          <a class="nav-link text-reset" href="#">HOME</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-reset" href="#">services</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-reset" href="#">skills</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-reset" href="#">partners</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-reset" href="#">blog</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-reset" href="#">get in touch</a>
+        </li>
+      </ul>
     </div>
-  </header>
+  </nav>
 </template>
 
 <script>
@@ -14,4 +32,8 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+nav {
+  color: #efefeeff;
+}
+</style>
