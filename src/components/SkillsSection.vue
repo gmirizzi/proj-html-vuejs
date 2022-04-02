f4e417068cdf602bb7352efa6319ed4c68821992
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-04-02 12:10:57 +0200

    skills section

--- src/components/SkillsSection.vue
+++ src/components/SkillsSection.vue
@@ -1,5 +1,20 @@
 <template>
-  <section id="skills"></section>
+  <section id="skills" class="bg-dark text-white">
+    <div class="container">
+      <div class="row">
+        <div class="col-6">
+          <div class="etichetta text-uppercase">about the network</div>
+          <h1>The Company</h1>
+          <p class="text-secondary">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+            quod, repudiandae consequatur cumque ullam corrupti error debitis
+            non fugit odio, molestiae, esse nisi laborum laboriosam aut nesciunt
+            recusandae distinctio repellendus?
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -8,4 +23,11 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+section {
+  background-image: url("./../assets/img/about-5.jpg");
+  background-position: right 0% bottom 50%;
+  background-size: 50%;
+  background-repeat: no-repeat;
+}
+</style>
