a75c97bb27bcf6a65bfcd4b62afc35cfb0bbb395
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-04-02 17:59:48 +0200

    creata la bar nel footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,11 +1,79 @@
 <template>
-  <footer></footer>
+  <footer class="pt-5 text-white">
+    <div class="container py-5">
+      <div class="row">
+        <div class="col-4">
+          <div id="contacts">
+            <h1>NEXGEN</h1>
+            <P
+              >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet
+              minus labore quo consequatur voluptatibus saepe et dolorum,
+              architecto rem ab obcaecati nihil, nostrum distinctio velit odio
+              quos itaque porro nulla!</P
+            >
+          </div>
+        </div>
+        <div class="col-8">
+          <div class="row">
+            <div class="col-4">
+              <div class="overlay h-100 w-100 p-4 rounded-3">
+                <h2>section</h2>
+                <ul>
+                  <li><a href="">link</a></li>
+                  <li><a href="">link</a></li>
+                  <li><a href="">link</a></li>
+                  <li><a href="">link</a></li>
+                  <li><a href="">link</a></li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-4">
+              <div class="overlay h-100 w-100 p-4 rounded-3">
+                <h2>section</h2>
+                <ul>
+                  <li><a href="">link</a></li>
+                  <li><a href="">link</a></li>
+                  <li><a href="">link</a></li>
+                  <li><a href="">link</a></li>
+                  <li><a href="">link</a></li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-4">
+              <div class="overlay h-100 w-100 p-4 rounded-3">
+                <h2>section</h2>
+                <ul>
+                  <li><a href="">link</a></li>
+                  <li><a href="">link</a></li>
+                  <li><a href="">link</a></li>
+                  <li><a href="">link</a></li>
+                  <li><a href="">link</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <footer-bar />
+  </footer>
 </template>
 
 <script>
+import FooterBar from "./FooterBar.vue";
 export default {
+  components: { FooterBar },
   name: "AppFooter",
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+footer {
+  background-image: url("./../assets/img/bg-10.jpg");
+  background-position: center;
+  background-size: cover;
+  .overlay {
+    background-color: rgba(0, 0, 0, 0.2);
+  }
+}
+</style>
---
+++ src/components/FooterBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="bg-dark text-white pt-4 pb-2 mt-5">
+    <div class="container d-flex justify-content-between">
+      <small
+        >Enjoy the low price. We are tracking any intention of piracy.</small
+      >
+      <small>2020 NEXGEN is Proudly Powered by <a href="#">Codings</a>.</small>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FooterBar",
+};
+</script>
+
+<style></style>
