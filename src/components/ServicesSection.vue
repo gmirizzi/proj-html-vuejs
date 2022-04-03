7d0fcf67c737f4a998286f040502a5cb82756850
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-04-03 14:28:23 +0200

    completato stile services section

--- src/components/ServicesSection.vue
+++ src/components/ServicesSection.vue
@@ -2,32 +2,32 @@
   <section id="services" class="py-5 d-flex align-items-center">
     <div class="container">
       <div class="row">
-        <div class="col-4">
+        <div class="col-4 pe-4">
           <div class="etichetta text-uppercase">our business area</div>
-          <h1 class="fw-bolder">Logistic services</h1>
-          <p class="text-secondary">
+          <h1 class="fw-bolder my-3">Logistics services</h1>
+          <p class="text-secondary pb-2">
             We are leaders in providing logistics services whit a set of cutting
             edge technologies and a team of experienced and renowed
             professionales
           </p>
-          <div>
+          <div id="services-btns" class="my-2">
             <button class="btn">Tradition</button
             ><button class="btn">Quality</button
             ><button class="btn">Security</button>
           </div>
-          <button class="btn text-uppercase">read more</button>
+          <button class="btn btn-primary text-uppercase mt-3">read more</button>
         </div>
         <div class="col-8">
           <div class="row">
             <div class="col-6">
-              <div class="card p-4">
+              <div class="card p-5">
                 <img
                   src="./../assets/img/gallery-3.jpg"
                   class="card-img-top"
                   alt="..."
                 />
                 <div class="card-body">
-                  <h5 class="card-title">Tecnlogy</h5>
+                  <h5 class="card-title fw-bold fs-3 my-3">Tecnology</h5>
                   <p class="card-text">
                     Some quick example text to build on the card title and make
                     up the bulk of the card's content.
@@ -36,14 +36,14 @@
               </div>
             </div>
             <div class="col-6">
-              <div class="card p-4">
+              <div class="card p-5">
                 <img
                   src="./../assets/img/gallery-5.jpg"
                   class="card-img-top"
                   alt="..."
                 />
                 <div class="card-body">
-                  <h5 class="card-title">Transport</h5>
+                  <h5 class="card-title fw-bold fs-3 my-3">Transport</h5>
                   <p class="card-text">
                     Some quick example text to build on the card title and make
                     up the bulk of the card's content.
@@ -65,7 +65,22 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "./../assets/css/_variables.scss";
+
 #services {
   background-color: #eef4edff;
+  .card {
+    border-radius: 1em;
+    border: none;
+  }
+  #services-btns {
+    .btn {
+      background-color: #cce3deff;
+      color: $blueLagoon;
+      padding-left: 0.5em;
+      padding-right: 0.5em;
+      margin-right: 1em;
+    }
+  }
 }
 </style>
