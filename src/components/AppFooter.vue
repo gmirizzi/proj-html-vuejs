e8ada8acaf2d19dd68fd0678db6f25988e4dda27
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-04-04 15:30:06 +0200

    generati i link nel footer dinamicamente

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -29,41 +29,19 @@
         </div>
         <div class="col-8">
           <div class="row">
-            <div class="col-4">
+            <div class="col-4" v-for="obj in arrLinks" :key="obj.section">
               <div class="overlay h-100 w-100 p-4 rounded-3">
-                <h2 class="fs-4 fw-bold text-white">section</h2>
+                <h2 class="fs-4 fw-bold text-white ms-3">{{ obj.section }}</h2>
                 <ul>
-                  <li><a class="text-secondary" href="">link</a></li>
-                  <li><a class="text-secondary" href="">link</a></li>
-                  <li><a class="text-secondary" href="">link</a></li>
-                  <li><a class="text-secondary" href="">link</a></li>
-                  <li><a class="text-secondary" href="">link</a></li>
-                </ul>
-              </div>
-            </div>
-            <div class="col-4">
-              <div class="overlay h-100 w-100 p-4 rounded-3">
-                <h2 class="fs-4 fw-bold text-white">section</h2>
-                <ul>
-                  <li><a class="text-secondary" href="">link</a></li>
-                  <li><a class="text-secondary" href="">link</a></li>
-                  <li><a class="text-secondary" href="">link</a></li>
-                  <li><a class="text-secondary" href="">link</a></li>
-                  <li><a class="text-secondary" href="">link</a></li>
-                </ul>
-              </div>
-            </div>
-            <div class="col-4">
-              <div class="overlay h-100 w-100 p-4 rounded-3">
-                <h2 class="fs-4 fw-bold text-white">section</h2>
-                <ul>
-                  <li><a class="text-secondary" href="">link</a></li>
-                  <li><a class="text-secondary" href="">link</a></li>
-                  <li><a class="text-secondary" href="">link</a></li>
-                  <li><a class="text-secondary" href="">link</a></li>
-                  <li class="text-secondary">
-                    <a class="ms-2 text-reset text-decoration-none" href=""
-                      >Information</a
+                  <li
+                    v-for="link in obj.links"
+                    :key="link.name"
+                    class="text-secondary"
+                  >
+                    <a
+                      class="ms-2 text-reset text-decoration-none"
+                      :href="link.src"
+                      >{{ link.name }}</a
                     >
                   </li>
                 </ul>
@@ -84,6 +62,99 @@ import NexgenBrand from "./NexgenBrand.vue";
 export default {
   components: { NexgenBrand, FooterBar },
   name: "AppFooter",
+  data() {
+    return {
+      arrLinks: [
+        {
+          section: "About",
+          links: [
+            {
+              name: "The Company",
+              src: "#",
+            },
+            {
+              name: "Institutional",
+              src: "#",
+            },
+            {
+              name: "Social & Events",
+              src: "#",
+            },
+            {
+              name: "Innovation",
+              src: "#",
+            },
+            {
+              name: "Enviroment",
+              src: "#",
+            },
+            {
+              name: "Technology",
+              src: "#",
+            },
+          ],
+        },
+        {
+          section: "Transport",
+          links: [
+            {
+              name: "Industrialized",
+              src: "#",
+            },
+            {
+              name: "Chemicals",
+              src: "#",
+            },
+            {
+              name: "Packaged Liquids",
+              src: "#",
+            },
+            {
+              name: "Constructions",
+              src: "#",
+            },
+            {
+              name: "Laminated Wood",
+              src: "#",
+            },
+            {
+              name: "And others",
+              src: "#",
+            },
+          ],
+        },
+        {
+          section: "Support",
+          links: [
+            {
+              name: "Responsability",
+              src: "#",
+            },
+            {
+              name: "Term of Use",
+              src: "#",
+            },
+            {
+              name: "About Cookies",
+              src: "#",
+            },
+            {
+              name: "Privacy Policy",
+              src: "#",
+            },
+            {
+              name: "Accessibility",
+              src: "#",
+            },
+            {
+              name: "Information",
+              src: "#",
+            },
+          ],
+        },
+      ],
+    };
+  },
 };
 </script>
 
